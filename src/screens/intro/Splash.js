1d41f3612ab3a5e72a0cf64785e3875dadb1776e
Ayushi-84 <kamthanayushi102@gmail.com>
2023-03-17 15:33:56 +0530

    Merge Changes

--- src/screens/intro/Splash.js
+++ src/screens/intro/Splash.js
@@ -15,7 +15,7 @@ export default function Splash(props) {
           props.navigation.reset({
             index: 0,
             // routes: [{name: 'ViewPager'}],
-            routes: [{name: 'Order'}],
+            routes: [{name: 'Dashboard'}],
           });
           // props.navigation.reset({
           //   index: 0,
