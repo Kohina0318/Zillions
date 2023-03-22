2fb1c2f05e1983f57c75ed69e14bd870d3558283
manish <prajapatimanish@3014.gmail.com>
2023-03-22 16:58:28 +0530

    merge

--- src/screens/Feedback/Feedback.js
+++ src/screens/Feedback/Feedback.js
@@ -17,6 +17,7 @@ import { useNavigation } from '@react-navigation/native';
 import HalfSizeButton from '../../components/shared/button/halfSizeButton';
 import { getProfileInfo } from '../../repository/ProfileRepository/ProfileRepo';
 import { getUserData } from '../../repository/CommonRepository';
+import { postFeedBack } from '../../repository/ProfileRepository/FeedBackRepo';
 
 const { width, height } = Dimensions.get('screen');
 
@@ -44,33 +45,115 @@ export default function FeedBack(props) {
   const themecolor = new MyThemeClass(mode).getThemeColor();
 
   const [loader, setLoader] = useState(true);
-  const [userName,setUserName]=useState("")
-  const [userMobileno,setUserMobileno]=useState("")
-  const [userEmail,setUserEmail]=useState("")
-  const [discription,setDiscription]=useState("")
+  const [userName, setUserName] = useState("")
+  const [userMobileno, setUserMobileno] = useState("")
+  const [userEmail, setUserEmail] = useState("")
+  const [discription, setDiscription] = useState("")
+
+
+  const handleSubmit = async () => {
+
+    if (userName == '') {
+      toast.show('Name is required!', {
+        type: 'warning',
+        placement: 'bottom',
+        duration: 3000,
+        offset: 30,
+        animationType: 'slide-in',
+      });
+    } else if (userMobileno == '') {
+      toast.show('Mobile No. is required!', {
+        type: 'warning',
+        placement: 'bottom',
+        duration: 3000,
+        offset: 30,
+        animationType: 'slide-in',
+      });
+    } else if (userMobileno.length < 10) {
+      toast.show('Please enter valid mobile number!', {
+        type: 'warning',
+        placement: 'bottom',
+        duration: 3000,
+        offset: 30,
+        animationType: 'slide-in',
+      });
+    } else if (userEmail == '') {
+      toast.show('Email is required!', {
+        type: 'warning',
+        placement: 'bottom',
+        duration: 3000,
+        offset: 30,
+        animationType: 'slide-in',
+      });
+    }
+    else if (discription == '') {
+      toast.show('Discription is required!', {
+        type: 'warning',
+        placement: 'bottom',
+        duration: 3000,
+        offset: 30,
+        animationType: 'slide-in',
+      });
+    }
+    else {
+      try {
+        let formdata = new FormData();
+        formdata.append('', userName);
+        formdata.append('', userMobileno);
+        formdata.append('', userEmail);
+        formdata.append('', discription);
+
+        console.log("formdata...", formdata)
+
+        var res = await postFeedBack(formdata);
+        if (res.status === true) {
+          toast.show(res.msg, {
+            type: 'success',
+            placement: 'bottom',
+            duration: 3000,
+            offset: 30,
+            animationType: 'slide-in',
+          });
+        }
+        else {
+
+        }
+
+      } catch (e) {
+        console.log('errrror in..getManageAddress page in address-->', e);
+        toast.show('Something went wrong!, Try again later.', {
+          type: 'danger',
+          placement: 'bottom',
+          duration: 3000,
+          offset: 30,
+          animationType: 'slide-in',
+        });
+      }
+    }
+  };
+
 
   useEffect(() => {
     async function temp() {
-        try {
-            var userData = await getUserData();
-            console.log(userData)
-            if (userData == null || userData == '' || userData == undefined) {
-                setLoader(false)
-           } else {    
-                var nameUser = `${userData[0].username.replace(/\s+/g, '')} ${userData[0].surname.replace(/\s+/g, '')}`
-                setUserName(nameUser)
-                setUserEmail(userData[0].email)
-                setUserMobileno(userData[0].phone)
-                setLoader(false)
-            }
-        } catch (e) {
+      try {
+        var userData = await getUserData();
+        if (userData == null || userData == '' || userData == undefined) {
+          setLoader(false)
+        } else {
+          var nameUser = `${userData[0].username.replace(/\s+/g, '')} ${userData[0].surname.replace(/\s+/g, '')}`
+          setUserName(nameUser)
+          setUserEmail(userData[0].email)
+          setUserMobileno(userData[0].phone)
           setLoader(false)
         }
+      } catch (e) {
+        setLoader(false)
+      }
     }
     temp()
-}, [props]);
+  }, [props]);
 
- 
+
   return (
     <View style={{ ...styles.bg, backgroundColor: themecolor.THEMECOLOR }}>
 
@@ -85,112 +168,112 @@ export default function FeedBack(props) {
       ) : (
         <>
           <View style={{ ...styles.container, }} >
-          <View style={{ ...styles.ViewHeading }}>
-                            <Text allowFontScaling={false} style={{ ...styles.headingTxt, color: themecolor.TXTWHITE }}>
-                                FeedBack
-                            </Text>
+            <View style={{ ...styles.ViewHeading }}>
+              <Text allowFontScaling={false} style={{ ...styles.headingTxt, color: themecolor.TXTWHITE }}>
+                FeedBack
+              </Text>
 
-                            <View
-                                style={{
-                                    ...styles.editInnerView,
-                                    backgroundColor: themecolor.BOXBORDERCOLOR,
-                                    borderColor: themecolor.BOXBORDERCOLOR1,
-                                }}>
-                            <View>  
-                            <Text allowFontScaling={false} style={{ ...styles.TextinputH, color: themecolor.TXTWHITE }}>Name</Text>
-                            <View
-                              style={{
-                                ...styles.TextView,
-                                borderColor: themecolor.BOXBORDERCOLOR1,
-                                backgroundColor: themecolor.BOXBORDERCOLOR,
-                              }}>
-                              <TextInput
-                                allowFontScaling={false}
-                                value={userName}
-                                placeholder={'Name'}
-                                placeholderTextColor={themecolor.TXTGREYS}
-                                style={{
-                                  ...styles.TextInput,
-                                  color: themecolor.TXTWHITE,
-                                }}
-                                onChangeText={txt => setUserName(txt)}
-                              />
-                            </View>
-                          </View>
-                          <View style={{ ...styles.Mv5 }} />
-                          <View>  
-                            <Text allowFontScaling={false} style={{ ...styles.TextinputH, color: themecolor.TXTWHITE }}>Mobile no</Text>
-                            <View
-                              style={{
-                                ...styles.TextView,
-                                borderColor: themecolor.BOXBORDERCOLOR1,
-                                backgroundColor: themecolor.BOXBORDERCOLOR,
-                              }}>
-                              <TextInput
-                                allowFontScaling={false}
-                                keyboardType="numeric"
-                                value={userMobileno}
-                                placeholder={'Mobile no'}
-                                placeholderTextColor={themecolor.TXTGREYS}
-                                style={{
-                                  ...styles.TextInput,
-                                  color: themecolor.TXTWHITE,
-                                }}
-                                onChangeText={txt => setUserMobileno(txt)}
-                              />
-                            </View>
-                          </View>
-                          <View style={{ ...styles.Mv5 }} />
-                          <View>  
-                            <Text allowFontScaling={false} style={{ ...styles.TextinputH, color: themecolor.TXTWHITE }}>Email</Text>
-                            <View
-                              style={{
-                                ...styles.TextView,
-                                borderColor: themecolor.BOXBORDERCOLOR1,
-                                backgroundColor: themecolor.BOXBORDERCOLOR,
-                              }}>
-                              <TextInput
-                                allowFontScaling={false}
-                                value={userEmail}
-                                placeholder={'Email'}
-                                placeholderTextColor={themecolor.TXTGREYS}
-                                style={{
-                                  ...styles.TextInput,
-                                  color: themecolor.TXTWHITE,
-                                }}
-                                onChangeText={txt => setUserEmail(txt)}
-                              />
-                            </View>
-                          </View>
-                          <View style={{ ...styles.Mv5 }} />
-                          <View>  
-                            <Text allowFontScaling={false} style={{ ...styles.TextinputH, color: themecolor.TXTWHITE }}>Discription</Text>
-                            <View
-                              style={{
-                                ...styles.modelTextViewMsg,
-                                borderColor: themecolor.BOXBORDERCOLOR1,
-                                backgroundColor: themecolor.BOXBORDERCOLOR,
-                              }}>
-                              <TextInput
-                                allowFontScaling={false}
-                                value={discription}
-                                multiline
-                                numberOfLines={4}
-                                placeholder={'Discription'}
-                                placeholderTextColor={themecolor.TXTGREYS}
-                                style={{
-                                  ...styles.modelTextInput,
-                                  color: themecolor.TXTWHITE,
-                                }}
-                                onChangeText={txt => setDiscription(txt)}
-                              />
-                            </View>
-                          </View>
+              <View
+                style={{
+                  ...styles.editInnerView,
+                  backgroundColor: themecolor.BOXBORDERCOLOR,
+                  borderColor: themecolor.BOXBORDERCOLOR1,
+                }}>
+                <View>
+                  <Text allowFontScaling={false} style={{ ...styles.TextinputH, color: themecolor.TXTWHITE }}>Name</Text>
+                  <View
+                    style={{
+                      ...styles.TextView,
+                      borderColor: themecolor.BOXBORDERCOLOR1,
+                      backgroundColor: themecolor.BOXBORDERCOLOR,
+                    }}>
+                    <TextInput
+                      allowFontScaling={false}
+                      value={userName}
+                      placeholder={'Name'}
+                      placeholderTextColor={themecolor.TXTGREYS}
+                      style={{
+                        ...styles.TextInput,
+                        color: themecolor.TXTWHITE,
+                      }}
+                      onChangeText={txt => setUserName(txt)}
+                    />
+                  </View>
+                </View>
+                <View style={{ ...styles.Mv5 }} />
+                <View>
+                  <Text allowFontScaling={false} style={{ ...styles.TextinputH, color: themecolor.TXTWHITE }}>Mobile no</Text>
+                  <View
+                    style={{
+                      ...styles.TextView,
+                      borderColor: themecolor.BOXBORDERCOLOR1,
+                      backgroundColor: themecolor.BOXBORDERCOLOR,
+                    }}>
+                    <TextInput
+                      allowFontScaling={false}
+                      keyboardType="numeric"
+                      value={userMobileno}
+                      placeholder={'Mobile no'}
+                      placeholderTextColor={themecolor.TXTGREYS}
+                      style={{
+                        ...styles.TextInput,
+                        color: themecolor.TXTWHITE,
+                      }}
+                      onChangeText={txt => setUserMobileno(txt)}
+                    />
+                  </View>
+                </View>
+                <View style={{ ...styles.Mv5 }} />
+                <View>
+                  <Text allowFontScaling={false} style={{ ...styles.TextinputH, color: themecolor.TXTWHITE }}>Email</Text>
+                  <View
+                    style={{
+                      ...styles.TextView,
+                      borderColor: themecolor.BOXBORDERCOLOR1,
+                      backgroundColor: themecolor.BOXBORDERCOLOR,
+                    }}>
+                    <TextInput
+                      allowFontScaling={false}
+                      value={userEmail}
+                      placeholder={'Email'}
+                      placeholderTextColor={themecolor.TXTGREYS}
+                      style={{
+                        ...styles.TextInput,
+                        color: themecolor.TXTWHITE,
+                      }}
+                      onChangeText={txt => setUserEmail(txt)}
+                    />
+                  </View>
+                </View>
+                <View style={{ ...styles.Mv5 }} />
+                <View>
+                  <Text allowFontScaling={false} style={{ ...styles.TextinputH, color: themecolor.TXTWHITE }}>Discription</Text>
+                  <View
+                    style={{
+                      ...styles.modelTextViewMsg,
+                      borderColor: themecolor.BOXBORDERCOLOR1,
+                      backgroundColor: themecolor.BOXBORDERCOLOR,
+                    }}>
+                    <TextInput
+                      allowFontScaling={false}
+                      value={discription}
+                      multiline
+                      numberOfLines={4}
+                      placeholder={'Discription'}
+                      placeholderTextColor={themecolor.TXTGREYS}
+                      style={{
+                        ...styles.modelTextInput,
+                        color: themecolor.TXTWHITE,
+                      }}
+                      onChangeText={txt => setDiscription(txt)}
+                    />
+                  </View>
+                </View>
 
 
-                                
-                                </View>
-                                <View style={{ ...styles.Mv5 }} />
+
+              </View>
+              <View style={{ ...styles.Mv5 }} />
 
               <View
                 style={{
@@ -203,16 +286,16 @@ export default function FeedBack(props) {
                     backgroundColor={themecolor.ADDTOCARTBUTTONCOLOR}
                     color={'#fff'}
                     borderColor={themecolor.BOXBORDERCOLOR1}
-                    onPress={() => handleEditProfile()}
+                    onPress={() => handleSubmit()}
                   />
                 </View>
               </View>
 
               <View style={{ marginVertical: 48 }} />
 
-                            </View>
+            </View>
           </View>
-          
+
 
         </>
 
