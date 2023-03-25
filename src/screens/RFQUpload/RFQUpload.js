80659bb08819da4067d89ea19a9820f873fa8814
manish <prajapatimanish@3014.gmail.com>
2023-03-25 11:32:44 +0530

    merge

--- src/screens/RFQUpload/RFQUpload.js
+++ src/screens/RFQUpload/RFQUpload.js
@@ -156,6 +156,15 @@ export default function RFQUpload(props) {
         animationType: 'slide-in',
       });
     }
+    else if ( !email.includes('@')|| !email.includes('gmail.com')) {
+      toast.show('Please enter valid email address!', {
+        type: 'warning',
+        placement: 'bottom',
+        duration: 3000,
+        offset: 30,
+        animationType: 'slide-in',
+      });
+    }
     else if (companyName == '') {
       toast.show('Company name is required!', {
         type: 'warning',
@@ -163,8 +172,16 @@ export default function RFQUpload(props) {
         duration: 3000,
         offset: 30,
         animationType: 'slide-in',
+      })
+    } 
+    else if (image == '') {
+      toast.show('Image is required!', {
+        type: 'warning',
+        placement: 'bottom',
+        duration: 3000,
+        offset: 30,
+        animationType: 'slide-in',
       });
-
     }
     else if (companyText == '') {
       toast.show('Company text is required!', {
@@ -183,7 +200,7 @@ export default function RFQUpload(props) {
         formdata.append('email', email);
         formdata.append('cname', companyName);
         formdata.append('commentText', commentText);
-
+        formdata.append('image',image)
         console.log("formdata...", formdata)
 
         var res = await postRFQUploadRepo(formdata);
